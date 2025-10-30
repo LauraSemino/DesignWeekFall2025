5158729ac2f75ca6586402c7cc0577ef7f46c149
Brandon <39204861+Bradash@users.noreply.github.com>
2025-10-30 13:49:41 -0400

    Shoot and Zombie sound added

--- DesignWeekFall2025/Assets/Scripts/EnemyAI.cs
+++ DesignWeekFall2025/Assets/Scripts/EnemyAI.cs
@@ -10,12 +10,18 @@ public class EnemyAI : MonoBehaviour
     GameObject currentTarget;
     public Animator animator;
     public ParticleSystem bloodParticles;
+    public AudioSource zombieSound;
+    public AudioClip[] AudioClip;
+    bool deathSound;
 
     public float health;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentTarget = entrance;
+        zombieSound.pitch = Random.Range(0.4f, 0.6f);
+        zombieSound.clip = AudioClip[Random.Range(8, 11)];
+        zombieSound.Play();
     }
 
     // Update is called once per frame
@@ -40,9 +46,16 @@ public class EnemyAI : MonoBehaviour
         //>fuckin dies lmao
         if (health <= 0)
         {
+            if (!deathSound)
+            {
+                zombieSound.pitch = Random.Range(0.4f, 0.6f);
+                zombieSound.clip = AudioClip[Random.Range(2, 7)];
+                zombieSound.Play();
+                deathSound = true;
+            }
             animator.SetBool("isDead", true);
             currentTarget = gameObject;
-            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("RealDeath"))
             {
                 Destroy(gameObject);
             }
@@ -52,5 +65,8 @@ public class EnemyAI : MonoBehaviour
     {
         bloodParticles.Play();
         animator.SetTrigger("hurt");
+        zombieSound.pitch = Random.Range(0.4f, 0.6f);
+        zombieSound.clip = AudioClip[Random.Range(0, 1)];
+        zombieSound.Play();
     }
 }
--- DesignWeekFall2025/Assets/Scripts/Projectile.cs
+++ DesignWeekFall2025/Assets/Scripts/Projectile.cs
@@ -8,10 +8,13 @@ public class Projectile : MonoBehaviour
     public float speed;
     public float damage;
     public float despawnTimer;
+    public AudioSource shootSound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         despawnTimer = 10f;
+        shootSound.pitch = Random.Range(0.8f, 1.2f);
+        shootSound.Play();
     }
 
     // Update is called once per frame
